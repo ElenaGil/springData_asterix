a6f63559455864189cdead428d9a394396ac40a2
ElenaGil <elenagillich@web.de>
2025-09-15 12:24:06 +0200

    Initial commit

---
+++ src/main/java/org/example/springdata_asterix/SpringDataAsterixApplication.java
@@ -0,0 +1,13 @@
+package org.example.springdata_asterix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataAsterixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataAsterixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/springdata_asterix/SpringDataAsterixApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springdata_asterix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataAsterixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
